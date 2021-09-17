009215dff1859f9d639f53a03475a2418c17c2c3
Benin B Sam <benin7895123@gmail.com>
2021-09-17 22:13:51 +0530

    ticket booking system

---
+++ main.js
@@ -0,0 +1,80 @@
+document.getElementById("hi").addEventListener("click", myFunction);
+document.getElementById("next").addEventListener("click", nextImg);
+document.getElementById("back").addEventListener("click", prevImg);
+document.getElementById("box");
+document.getElementById("login").addEventListener("click",scroll);
+document.getElementById("submit").addEventListener("click",getUserDetails);
+
+setInterval(nextImg, 2000)
+var imageNo = 1;
+
+function myFunction() {
+ alert('hi');
+}
+
+function changeImage() {
+    var image = document.getElementById('box');
+    if (image.src.match("iplane.jpg")) {
+        image.src = "itrain.jpg";
+    }
+    else {
+        image.src = "iplane.jpg";
+    }
+}
+
+function nextImg(){
+    var image = document.getElementById('box');
+    if(imageNo==0) {
+        image.src="iplane.jpg";
+        imageNo++;
+    }
+    else if(imageNo===1){
+        image.src="itrain.jpg";
+        imageNo++;
+    }else if(imageNo===2){
+        image.src="ibus.jpg";
+        imageNo++;
+    }else if(imageNo===3){
+        image.src="itheatre.jpg";
+        imageNo = 0 ;    // image set to zero to loop it 
+    }
+    
+}
+
+function prevImg(){
+    var image = document.getElementById('box');
+    if (image.src.match("iplane.jpg")) {
+        image.src = "itheatre.jpg";
+        imageNo--;
+    }else if(image.src.match("itrain.jpg")){
+        image.src = "iplane.jpg";
+        imageNo--;
+    }else if(image.src.match("ibus.jpg")){
+        image.src = "itrain.jpg";
+        imageNo--;
+    }else if(image.src.match("itheatre.jpg")){
+        image.src = "ibus.jpg";
+        //imageNo--;
+    }
+    
+}
+
+
+function scroll(){
+    window.scrollTo(0,300);
+    } 
+
+
+ function getUserDetails(){
+     
+     var x = document.getElementById("iUserEmail").value;
+     document.getElementById("check").innerHTML = x;
+     /*<h1 class="heading">Make your<br> reservation </h1>
+     <h2  class="subheading">with us you can easily book any ticket  you need<br> 
+         to travel safely , thanks to our detailed sytem of searching<br> and booking tickets</h2>*/
+     
+    // x = Document.getElementById("iUserEmail").value;
+    //x=Number(document.getElementById("iUserEmail").value);
+    //document.getElementById("check").value = 10;
+}   
+
